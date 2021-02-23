dcef1e46fb3d307d55be07894577aac2a860fe17
HamzahSami <hamzahsami@gmail.com>
2021-02-23 14:08:33 -0800

    Update stat 482 final project.R

--- Predicting Housing Prices/stat 482 final project.R
+++ Predicting Housing Prices/stat 482 final project.R
@@ -1,25 +1,54 @@
 data <- read.csv(file = "~/Documents/csulb statistics/stat 482/housing.csv", header = TRUE, sep = ",")
 housing <- data$prices
 
-
-
-housing_prices <- c(610000,799999,814999,739000,850000,899000,779000,700000,550000,689000,925000,1300000,711000,950000,585000,620000,529000,850000,899000,1275000,835000,1449900,649000,699900,663000,674800,799999,675000,725000,1495000,899000,950000,799000,1199000,1599000,900000)
 hist(log(housing_prices),col= 'orange',xlab = "Housing Prices", ylab = "Number of Houses")
-mean(housing_prices)
 
-#normally distributed
+#Expected Value and Standard Deviation
+house_avg = mean(housing)
+house_var = var(housing)
+
+#Verify if data is normally distributed
 shapiro.test(log(housing_prices))
 
-
-
-
-#lambda:
+#Given total homes sold, designate this as lambda.
 lambda = 46/365
 
+#Simulate trajectories for an entire year.
+t<- 365
+lambda<- 46/61
 
-#Find expected value
+#generating N(t)
+set.seed(12345)
+Nt<- rpois(1,lambda*t)
 
-#Find standard deviation
+#generating N(t) standard uniforms
+unif<- 1:Nt
+for(i in 1:Nt)
+  u[i]<- runif(1) 
 
-#simulate 100 trajectories
+#sorting standard uniforms
+sort_unif<- sort(unif)
 
+#computing N(t) event times
+events<- t* sort_unif
+
+#simulating trajectory for one year (365 days)
+time<- 1:2*Nt
+profit<- 1:2*Nt
+time[1]<- 0
+profit[1]<- 0
+for (i in seq(2, 2*Nt, 2)) {
+  time[i]<- events[i/2]-0.001
+  time[i+1]<- events[i/2]
+  profit[i]<- profit[i-1]
+  profit[i+1]<- profit[i-1]+(abs(rnorm(1)))
+}
+
+#plotting simulated trajectory
+plot(time, profit, type='l', col=4, panel.first=grid())
+
+#estimated number of homes sold within a year
+Nt
+
+#Estimated gross profit of homes sold 
+profit[length(profit)]
